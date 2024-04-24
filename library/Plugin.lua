b3bbed16b14bd69ccb8ff3e17d54730204184214
IoriBranford <ioribranford@gmail.com>
2024-04-24 14:34:25 -0700

    Document plugin init and exit functions.

--- library/Plugin.lua
+++ library/Plugin.lua
@@ -8,3 +8,9 @@ Plugin = {}
 
 ---@param t {id:string, title:string, group:string, onclick:function, onenabled:fun():boolean}
 function Plugin:newCommand(t) end
+
+---@param plugin Plugin
+function init(plugin) end
+
+---@param plugin Plugin
+function exit(plugin) end
