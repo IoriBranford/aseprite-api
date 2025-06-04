d871f7f1775edfa2bc9dcfd705f0b56538affdd9
IoriBranford <ioribranford@gmail.com>
2025-06-04 14:28:03 -0700

    Move Palette = {} under class desc

--- library/Palette.lua
+++ library/Palette.lua
@@ -3,6 +3,7 @@
 ---@class Palette
 ---@field frame Frame At the moment it always return the first frame, but in a near future Aseprite will support palette changes over time (in different frames), so this field should be the frame number where this palette is displayed for first time in the sprite.
 ---@operator len:integer
+Palette = {}
 
 ---Creates a new palette. By default it will contain 256 colors
 ---@overload fun(numberOfColors:integer):Palette
@@ -10,8 +11,6 @@
 ---@overload fun(t:{fromFile:string?, fromResource:string?}):Palette If fromFile is given, the palette is loaded from the file name. If fromResource is given, the resourceID is an ID specified in one of the extensions palette (e.g. DB16, DB32, Solarized).
 ---@return Palette
 function Palette() end
-
-Palette = {}
 
 ---@param numberOfColors integer
 function Palette:resize(numberOfColors) end
