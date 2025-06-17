f8f3f14e274ee8880e111b468ba0a6997c8f1417
IoriBranford <ioribranford@gmail.com>
2025-06-17 15:30:18 -0700

    filePath, fileName, fileTitle

--- library/app.fs.lua
+++ library/app.fs.lua
@@ -4,4 +4,16 @@ app.fs = {}
 
 ---@param fn string
 ---@return string
+function app.fs.filePath(fn) end
+
+---@param fn string
+---@return string
+function app.fs.fileName(fn) end
+
+---@param fn string
+---@return string
+function app.fs.fileTitle(fn) end
+
+---@param fn string
+---@return string
 function app.fs.filePathAndTitle(fn) end
