ab64e5925d41ef03310e6fc1204e63835af6f375
IoriBranford <ioribranford@gmail.com>
2025-06-17 23:16:06 -0700

    RectangleArg and SizeArg

--- library/Geometry.lua
+++ library/Geometry.lua
@@ -15,7 +15,9 @@ function Point() end
 ---@field height number
 Size = {}
 
----@param otherSize Size
+---@alias SizeArg {w:number, h:number}|{width:number, height:number}|number[]|Size
+
+---@param otherSize SizeArg
 ---@return Size newSize which will be big enough to contains both given dimensions (self and otherSize).
 function Size:union(otherSize) end
 
@@ -27,34 +29,33 @@ function Size:union(otherSize) end
 ---@field width number
 ---@field height number
 ---@field origin Point
----@field size Size
+---@field size SizeArg
 ---@field isEmpty boolean
 Rectangle = {}
+
+---@alias RectangleArg Rectangle|number[]|{x:number, y:number, width:number, height:number}|{x:number, y:number, wait:number, h:number}
 
 ---@param x number
 ---@param y number
 ---@param w number
 ---@param h number
 ---@return Rectangle
----@overload fun(r:Rectangle):Rectangle
----@overload fun(t:{x:number,y:number,w:number,h:number}):Rectangle
----@overload fun(t:{x:number,y:number,width:number,height:number}):Rectangle
----@overload fun(t:number[]):Rectangle
+---@overload fun(r:RectangleArg):Rectangle
 function Rectangle(x, y, w, h) end
 
----@param otherRectangle Rectangle
+---@param otherRectangle RectangleArg
 ---@return boolean
 function Rectangle:contains(otherRectangle) end
 
----@param otherRectangle Rectangle
+---@param otherRectangle RectangleArg
 ---@return boolean
 function Rectangle:intersects(otherRectangle) end
 
----@param otherRectangle Rectangle
+---@param otherRectangle RectangleArg
 ---@return Rectangle newRectangle which is the intersection of rectangle and otherRectangle. If both rectangles don't intersect each other, the result will be an empty rectangle
 function Rectangle:intersect(otherRectangle) end
 
----@param otherRectangle Rectangle
+---@param otherRectangle RectangleArg
 ---@return Rectangle newRectangle which will be a rectangle big enough to contains both given rectangles rectangle and otherRectangle.
 function Rectangle:union(otherRectangle) end
 
