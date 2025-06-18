d65df22ae1e0b255931aeb31a1e17c8206576d0e
IoriBranford <ioribranford@gmail.com>
2025-06-17 23:19:44 -0700

    joinPath, isFile, isDirectory, listFiles

--- library/app.fs.lua
+++ library/app.fs.lua
@@ -17,3 +17,21 @@ function app.fs.fileTitle(fn) end
 ---@param fn string
 ---@return string
 function app.fs.filePathAndTitle(fn) end
+
+---Can accept any number of string arguments to join together with the path separator for the current platform.
+---@param ... string
+---@return string
+function app.fs.joinPath(...) end
+
+---@param fn string
+---@return boolean
+function app.fs.isFile(fn) end
+
+---@param fn string
+---@return boolean
+function app.fs.isDirectory(fn) end
+
+---Returns a list of files in the given directory path
+---@param path string
+---@return string[] files a table where each element is a file name, each file name is relative to the given path, they are not full path file names.
+function app.fs.listFiles(path) end
